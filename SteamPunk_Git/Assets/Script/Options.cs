dd44e515350ef43d4de63a2f68a81fd51e6dffb2
changyowen <changyowen@gmail.com>
2020-08-18 14:09:41 +0800

    First Commit

---
+++ SteamPunk_Git/Assets/Script/Options.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public GameObject optionpanel;
+    public GameObject audioSource;
+    public static bool GamePaused = false;
+    public Toggle musicToggle;
+    public AudioSource musicSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionpanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            if(GamePaused)
+            {
+                Unpause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Debug.Log("Paused");
+        Time.timeScale = 0f;
+        optionpanel.SetActive(true);
+        GamePaused = true;
+    }
+
+    public void Unpause()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log("sozai");
+        Time.timeScale = 1f;
+        optionpanel.SetActive(false);
+        GamePaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Main menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("die");
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+
+    public void ToggleMusic()
+    {
+        if(musicSource==false)
+        {
+            musicSource.UnPause();
+        }
+        else if (musicSource == true)
+        {
+            musicSource.Pause();
+        }
+    }
+}
