b66869f94057325e885b9a7cf0010efb28a014dc
changyowen <changyowen@gmail.com>
2020-08-22 00:09:08 +0800

    Revert "Revert "Update 21/8/2020 9.24pm""
    This reverts commit aaecdf00a8a396e8a51a9a27ce77819adb231b30.

---
+++ SteamPunk_Git/Assets/TestMaterialScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMaterialScript : MonoBehaviour
+{
+    Renderer meshRenderer;
+    public Material instanceMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<Renderer>();
+        instanceMaterial = meshRenderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float intensity = 100.0f;
+        instanceMaterial.EnableKeyword("_EMISSION");
+        instanceMaterial.SetColor("_Color", new Color(1f, 0f, 0f, 1.0f));
+        //instanceMaterial.SetColor("_EmissionColor", Color.blue * 1f);
+
+
+
+        instanceMaterial.SetColor("_EmissionColor", new Color(1.0f, 0f, 0f, 1.0f) * intensity);
+    }
+}
